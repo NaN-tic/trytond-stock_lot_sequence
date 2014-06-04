fe8a66c57fbf50268b99e24fc7c7568c31d8a23d
Guillem Barba <guillem@nan-tic.com>
2014-06-04 11:32:48 +0200

    Fix get/set_configuration functions of stock.configuration and removed problematic required in stock.configuration.company field

--- stock.py
+++ stock.py
@@ -1,6 +1,6 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.model import ModelSQL, fields
+from trytond.model import Model, ModelSQL, fields
 from trytond.pyson import Eval
 from trytond.pool import Pool, PoolMeta
 from trytond.transaction import Transaction
@@ -13,13 +13,13 @@ class CompanyConfiguration(ModelSQL):
     'Stock Company Configuration'
     __name__ = 'stock.configuration.company'
 
-    company = fields.Many2One('company.company', 'Company', required=True)
+    company = fields.Many2One('company.company', 'Company', required=True,
+        ondelete='CASCADE', select=True)
     lot_sequence = fields.Many2One('ir.sequence', 'Lot Sequence', domain=[
             ('code', '=', 'stock.lot'),
             ('company', 'in',
                 [Eval('context', {}).get('company', -1), None]),
-            ],
-        required=True)
+            ])
 
     @staticmethod
     def default_company():
@@ -34,34 +34,44 @@ class Configuration:
                 ('code', '=', 'stock.lot'),
                 ('company', 'in',
                     [Eval('context', {}).get('company', -1), None]),
-                ],
-        required=True), 'get_configuration', setter='set_configuration')
+                ], required=True),
+        'get_configuration', setter='set_configuration')
 
     @classmethod
-    def get_configuration(self, configs, names):
+    def get_configuration(cls, configs, names):
         pool = Pool()
         CompanyConfig = pool.get('stock.configuration.company')
-        res = dict.fromkeys(names, [configs[0].id, None])
-        company_configs = CompanyConfig.search([], limit=1)
-        if len(company_configs) == 1:
-            company_config, = company_configs
-            for field_name in set(names):
-                value = getattr(company_config, field_name, None)
-                if value:
-                    res[field_name] = {configs[0].id: value.id}
+
+        company_id = Transaction().context.get('company')
+        company_configs = CompanyConfig.search([
+                ('company', '=', company_id),
+                ])
+
+        res = {}
+        for fname in names:
+            res[fname] = {}.fromkeys([c.id for c in configs], None)
+            if company_configs:
+                val = getattr(company_configs[0], fname)
+                if isinstance(val, Model):
+                    val = val.id
+                res[fname][configs[0].id] = val
         return res
 
     @classmethod
-    def set_configuration(self, configs, name, value):
+    def set_configuration(cls, configs, name, value):
         pool = Pool()
         CompanyConfig = pool.get('stock.configuration.company')
-        company_configs = CompanyConfig.search([], limit=1)
-        if len(company_configs) == 1:
-            company_config, = company_configs
+
+        company_id = Transaction().context.get('company')
+        company_configs = CompanyConfig.search([
+                ('company', '=', company_id),
+                ])
+        if company_configs:
+            company_config = company_configs[0]
         else:
-            company_config = CompanyConfig()
-            setattr(company_config, name, value)
-            company_config.save()
+            company_config = CompanyConfig(company=company_id)
+        setattr(company_config, name, value)
+        company_config.save()
 
 
 class Lot:
@@ -73,9 +83,9 @@ class Lot:
         if cls.number.required:
             cls.number.required = False
         cls._error_messages.update({
-                'no_sequence':  'No sequence defined for lot. You must define'
-                ' a sequence or enter lot\'s number.',
-            })
+                'no_sequence':  ('No sequence defined for lot. You must '
+                    'define a sequence or enter lot\'s number.'),
+                })
 
     @classmethod
     def create(cls, vlist):
