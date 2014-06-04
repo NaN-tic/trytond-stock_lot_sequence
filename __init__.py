ee52054446a81b2adb1f3a20acfb3e750f819f08
Guillem Barba <guillem@nan-tic.com>
2014-06-04 11:32:58 +0200

    Increase version to 3.2.0

--- __init__.py
+++ __init__.py
@@ -1,6 +1,5 @@
-#The COPYRIGHT file at the top level of this repository contains the full
-#copyright notices and license terms.
-
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 from trytond.pool import Pool
 from stock import *
 
