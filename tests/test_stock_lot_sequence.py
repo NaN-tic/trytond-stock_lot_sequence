b486be5fc6263b4a7be41ab14aa62141185cba25
Sergi Almacellas Abellana <sergi@koolpi.com>
2014-11-05 13:33:54 +0100

    Migrate to version 3.4

--- tests/test_stock_lot_sequence.py
+++ tests/test_stock_lot_sequence.py
@@ -7,23 +7,17 @@ from trytond.tests.test_tryton import test_view, test_depends
 
 
 class TestCase(unittest.TestCase):
-    '''
-    Test module.
-    '''
+    'Test module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('stock_lot_sequence')
 
     def test0005views(self):
-        '''
-        Test views.
-        '''
+        'Test views'
         test_view('stock_lot_sequence')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
