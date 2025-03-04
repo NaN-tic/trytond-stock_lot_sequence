319305f839802a6e098759533b7134380afe1199
Raimon Esteve <raimonesteve@gmail.com>
2025-03-04 14:22:58 +0100

    Migrate MultiValue table to lot_sequence data From changeset 841d27f

--- product.py
+++ product.py
@@ -1,5 +1,7 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
+from sql import  Null, Table
+from trytond.transaction import Transaction
 from trytond.model import ModelSQL, fields
 from trytond.pool import PoolMeta, Pool
 from trytond.pyson import Eval, Id
@@ -67,3 +69,44 @@ class Category(CompanyMultiValueMixin, metaclass=PoolMeta):
             ]))
     lot_sequences = fields.One2Many('product.category.lot_sequence',
         'category', 'Lot Sequences')
+
+
+class Template(metaclass=PoolMeta):
+    __name__ = 'product.template'
+
+    @classmethod
+    def __register__(cls, module_name):
+        pool = Pool()
+        Sequence = pool.get('ir.sequence')
+
+        exist = backend.TableHandler.table_exist('product_template_lot_sequence')
+
+        super().__register__(module_name)
+
+        if exist:
+            lot_sequence = Table('product_template_lot_sequence')
+            template = cls.__table__()
+            sequence = Sequence.__table__()
+            cursor = Transaction().connection.cursor()
+
+            query = lot_sequence.select(lot_sequence.lot_sequence, lot_sequence.template, where=lot_sequence.lot_sequence != Null)
+            cursor.execute(*query)
+
+            sequence_ids = set()
+            for row in cursor.fetchall():
+                sequence_id = row[0]
+                template_id = row[1]
+                cursor.execute(*template.update(
+                        columns=[template.lot_sequence],
+                        values=[sequence_id],
+                        where=template.id == template_id))
+                sequence_ids.add(sequence_id)
+
+            if sequence_ids:
+                cursor.execute(*sequence.update(
+                        columns=[sequence.company],
+                        values=[Null],
+                        where=sequence.id.in_(list(sequence_ids))))
+
+            backend.TableHandler.table_rename('product_template_lot_sequence',
+                'product_template_lot_sequence_back')
